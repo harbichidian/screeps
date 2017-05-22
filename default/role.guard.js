fdb210301880e51affe9de9dc5b2a7847a8b51c1
Josh Baker <jtbaker@jtbaker.name>
2017-05-21 21:46:42 -0700

    Define some actual guard behavior

--- default/role.guard.js
+++ default/role.guard.js
@@ -1,20 +1,22 @@
 module.exports = {
 	run: function(creep) {
-		if(creep.memory.target) {
-			switch(creep.attack(creep.memory.target)) {
-				case ERR_NOT_IN_RANGE:
-					creep.moveTo(target);
-					break;
-				case ERR_INVALID_TARGET:
-					if(creep.memory.target.hits == 0) creep.memory.target = null;
-					break;
-			}
-		}
-		else {
-			creep.memory.target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
-			if(!creep.memory.target) {
-				// Try to heal
-			}
+		creep.memory.target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS) || creep.pos.findClosestByRange(FIND_SOURCES);
+		
+		switch(typeof creep.memory.target) {
+			case "Creep":
+				switch(creep.attack(creep.memory.target)) {
+					case ERR_NOT_IN_RANGE:
+						creep.moveTo(target);
+						break;
+					case ERR_INVALID_TARGET:
+						if(creep.memory.target.hits == 0) creep.memory.target = null;
+						break;
+				}
+				break;
+			case "Source":
+				// Hover near sources, because that's probably where our creeps will be
+				if(creep.pos.getRangeTo(creep.memory.target) > 2) creep.moveTo(target);
+				break;
 		}
 	}
 };
