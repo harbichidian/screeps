154b31bfa169f2b88ad21910b5496b3b3bb9cc94
Josh Baker <jtbaker@jtbaker.name>
2017-06-01 21:15:37 -0700

    .

--- default/role.guard.js
+++ default/role.guard.js
@@ -2,9 +2,9 @@ module.exports.body = [MOVE, ATTACK, ATTACK];
 
 module.exports.run = function(creep) {
 	if(creep.memory.target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS)) {
-	    console.log(`${creep.name} found an enemy!`);
-	    
-	    switch(creep.attack(creep.memory.target)) {
+		console.log(`${creep.name} found an enemy!`);
+		
+		switch(creep.attack(creep.memory.target)) {
 			case ERR_NOT_IN_RANGE:
 				creep.moveTo(creep.memory.target);
 				break;
@@ -14,7 +14,7 @@ module.exports.run = function(creep) {
 		}
 	}
 	else if(creep.memory.target = creep.pos.findClosestByRange(FIND_SOURCES)) {
-	    // Hover near sources, because that's probably where our creeps will be
-		if(creep.pos.getRangeTo(creep.memory.target) > 4) creep.moveTo(target);
+		// Hover near sources, because that's probably where our creeps will be
+		if(creep.pos.getRangeTo(creep.memory.target) > 4) creep.moveTo(creep.memory.target);
 	}
 };
