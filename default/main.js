6343b948a94c4eb4bb5471d40a207f150f1fefeb
Josh Baker <jtbaker@jtbaker.name>
2017-06-01 21:14:24 -0700

    It only took me a billion tries!

--- default/main.js
+++ default/main.js
@@ -1,27 +1,27 @@
-Memory.roles = {
+var $roles = {
 	harvester: require("role.harvester"),
 	guard: require("role.guard")
 };
 
-Memory.rolePercentages = {
+var $rolePercentages = {
 	harvester: 0.85,
 	guard: 0.15
 };
 
 module.exports.loop = function() {
 	// Keep at least 5 creeps around
-	if(Object.keys(Game.creeps).length < 5) {
+	if(Object.keys(Game.creeps).length < 10) {
 		var underrepresentation = {};
-		Object.keys(Memory.rolePercentages).forEach(function(role) {
+		Object.keys($rolePercentages).forEach(function(role) {
 			underrepresentation[role] = Object.keys(Game.creeps).filter(n => Game.creeps[n].role == role).length / Object.keys(Game.creeps).length;
 		});
 		
 		var roleToSpawn = Object.keys(underrepresentation).sort((a, b) => underrepresentation[a] - underrepresentation[b]).pop();
 		
-		switch(Game.spawns["Spawn1"].canCreateCreep(Memory.roles[roleToSpawn].body)) {
+		switch(Game.spawns["Spawn1"].canCreateCreep($roles[roleToSpawn].body)) {
 			case OK:
 				console.log(`Spawn1 spawning ${roleToSpawn}`);
-				Game.spawns['Spawn1'].createCreep(Memory.roles[roleToSpawn].body, undefined, {role: roleToSpawn});
+				Game.spawns['Spawn1'].createCreep($roles[roleToSpawn].body, undefined, {role: roleToSpawn});
 				break;
 			case ERR_NOT_ENOUGH_ENERGY:
 				console.log("Spawn1 waiting for energy");
@@ -30,6 +30,14 @@ module.exports.loop = function() {
 	}
 	
 	Object.keys(Game.creeps).forEach(function(n) {
-		Memory.roles[Game.creeps[n].memory.role].run(Game.creeps[n]);
+	    console.log(`Running ${n}'s code, which is for role:${Game.creeps[n].memory.role}`);
+	    
+		var role = $roles[Game.creeps[n].memory.role];
+		if(role.run) {
+		    role.run(Game.creeps[n]);
+		}
+		else {
+		    console.log(`${Game.creeps[n].memory.role}.run was false`);
+		}
 	});
 }
--- default/role.guard.js
+++ default/role.guard.js
@@ -1,23 +1,20 @@
-module.exports = {
-	body: [MOVE, CARRY, WORK],
-	run: function(creep) {
-		creep.memory.target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS) || creep.pos.findClosestByRange(FIND_SOURCES);
-		
-		switch(typeof creep.memory.target) {
-			case "Creep":
-				switch(creep.attack(creep.memory.target)) {
-					case ERR_NOT_IN_RANGE:
-						creep.moveTo(target);
-						break;
-					case ERR_INVALID_TARGET:
-						if(creep.memory.target.hits == 0) creep.memory.target = null;
-						break;
-				}
+module.exports.body = [MOVE, ATTACK, ATTACK];
+
+module.exports.run = function(creep) {
+	if(creep.memory.target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS)) {
+	    console.log(`${creep.name} found an enemy!`);
+	    
+	    switch(creep.attack(creep.memory.target)) {
+			case ERR_NOT_IN_RANGE:
+				creep.moveTo(creep.memory.target);
 				break;
-			case "Source":
-				// Hover near sources, because that's probably where our creeps will be
-				if(creep.pos.getRangeTo(creep.memory.target) > 4) creep.moveTo(target);
+			case ERR_INVALID_TARGET:
+				if(creep.memory.target.hits == 0) creep.memory.target = null;
 				break;
 		}
+	}
+	else if(creep.memory.target = creep.pos.findClosestByRange(FIND_SOURCES)) {
+	    // Hover near sources, because that's probably where our creeps will be
+		if(creep.pos.getRangeTo(creep.memory.target) > 4) creep.moveTo(target);
 	}
 };
